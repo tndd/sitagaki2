0fc48bb0ccf126275bd22a7872521c62b24dbf75
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-06 18:51:01 +0900

    データ読み込み処理の関数化

--- src/dataset/repository.py
+++ src/dataset/repository.py
@@ -2,20 +2,36 @@ from os import path
 
 import pandas as pd
 
-# 現在のスクリプトファイルのディレクトリを取得
-self_path = path.dirname(path.abspath(__file__))
+# 現在のスクリプトファイルのディレクトリパス
+SELF_PATH = path.dirname(path.abspath(__file__))
 
-# スクリプトのディレクトリを基準にデータファイルのパスを構築
-data_path = path.join(self_path, "data", "AAPL.csv")
 
-# CSVファイルを読み込む
-df = pd.read_csv(
-    data_path,
-    parse_dates=["Date"],
-    date_format="%m/%d/%Y",
-    index_col="Date",
-)
+def _read_df(name: str):
+    """
+    dataset/dataディレクトリ下のcsvファイルの読み出し
+    """
+    data_path = path.join(SELF_PATH, "data", f"{name}.csv")
+    return pd.read_csv(
+        data_path,
+        parse_dates=["Date"],
+        date_format="%m/%d/%Y",
+        index_col="Date",
+    )
 
-# データを日付順にソート
-df = df.sort_index()
-print(df)
+
+def read_df_aapl():
+    return _read_df("AAPL")
+
+
+def read_df_amd():
+    return _read_df("AMD")
+
+
+def read_df_sbux():
+    return _read_df("SBUX")
+
+
+if __name__ == "__main__":
+    # データを日付順にソート
+    df = read_df_aapl()
+    print(df)
