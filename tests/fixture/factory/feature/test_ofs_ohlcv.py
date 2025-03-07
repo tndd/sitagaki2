7c4d611d21984369f509ed64bfc5edf3eaee6b02
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-07 21:24:32 +0900

    add todo

--- tests/fixture/factory/feature/test_ofs_ohlcv.py
+++ tests/fixture/factory/feature/test_ofs_ohlcv.py
@@ -5,3 +5,4 @@ from fixtures.factory.feature.ofs_ohlcv import factory_ofs_ohlcv
 def test_factory_ofs_ohlcv():
     ofs_ohlcv = factory_ofs_ohlcv()
     assert isinstance(ofs_ohlcv, OFS_OHLCV)
+    # TODO: ofs_ohlcvの内容確認処理
