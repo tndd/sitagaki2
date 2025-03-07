65cbb5d4df2599ce6fbc441a2bd063bade7a169c
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-07 15:10:45 +0900

    子側でselectするのとコメント拡充
    コメントで中途DFを説明するのは、ちょっと妥協的ではある。けどこれ以上深入りはしない。

--- src/feature/repository.py
+++ src/feature/repository.py
@@ -8,6 +8,13 @@ COLS_HLC = ("High", "Low", "Close")
 
 
 def _log_valiation_volume_from_open(df: OHCLV):
+    """
+    前日を基準としたOpenとVolumeの対数差分。
+
+    DF:
+        OpenOfs     f64     前日Openからの対数差分
+        VolumeOfs   f64     前日Volumeからの対数差分
+    """
     return df.select(
         [
             (pl.col("Open") / pl.col("Open").shift(1)).log().alias("OpenOfs") * 10000,
@@ -18,28 +25,33 @@ def _log_valiation_volume_from_open(df: OHCLV):
 
 
 def _log_valiation_high_low_close_from_open(df: OHCLV):
+    """
+    当日Openを基準としたHigh,Low,Closeの対数差分。
+
+    DF:
+        HighOfs     f64     当日OpenからHighへの対数差分
+        LowOfs      f64     当日OpenからLowへの対数差分
+        CloseOfs    f64     当日OpenからのCloseへの対数差分
+    """
     return df.with_columns(
         [
             (pl.col(col) / pl.col("Open")).log().alias(f"{col}Ofs") * 10000
             for col in COLS_HLC
         ]
-    )
+    ).select([f"{col}Ofs" for col in COLS_HLC])
 
 
 def derive_ofs_ohclv(df: OHCLV) -> OFS_OHCLV:
-    df_ofs_ov = _log_valiation_volume_from_open(df)
-    df_ofs_hlc = _log_valiation_high_low_close_from_open(df)
-    # 結合して1行目を削除（shiftでNaNになるため）
     return (
         pl.concat(
             [
                 df.select("Date"),
-                df_ofs_ov,
-                df_ofs_hlc.select([f"{col}Ofs" for col in COLS_HLC]),
+                _log_valiation_volume_from_open(df),
+                _log_valiation_high_low_close_from_open(df),
             ],
             how="horizontal",
         )
-        .slice(1)
+        .slice(1)  # 結合して1行目を削除（shiftでNaNになるため）
         .select(OFS_OHCLV_PLDF.columns)
     )
 
