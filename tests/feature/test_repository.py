a4728bbff2d2f387ed982fea00b1148132d00aec
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-07 21:21:06 +0900

    pythonpath追加

--- tests/feature/test_repository.py
+++ tests/feature/test_repository.py
@@ -4,6 +4,7 @@ from feature.repository import derive_df_ofs_ohlcv
 
 
 def test_derive_df_ofs_ohlcv():
+    pass
     # テスト用のOHLCVに則ったデータフレームを作成
-    df = pl.DataFrame()
-    df_ofs = derive_df_ofs_ohlcv(df)
+    # df = pl.DataFrame()
+    # df_ofs = derive_df_ofs_ohlcv(df)
