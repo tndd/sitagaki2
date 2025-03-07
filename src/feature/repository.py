eba71e9f29fee9383e25de3e0aa4ea21c218024c
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-07 14:37:33 +0900

    calc_ofs_ohclvの実装
    テスト未実装

---
+++ src/feature/repository.py
@@ -0,0 +1,50 @@
+import polars as pl
+
+from dataset.repository import read_df_aapl
+from dataset.schema import OHCLV
+from feature.schema import OFS_OHCLV, OFS_OHCLV_PLDF
+
+COLS_HLC = ("High", "Low", "Close")
+
+
+def _log_valiation_volume_from_open(df: OHCLV):
+    return df.select(
+        [
+            (pl.col("Open") / pl.col("Open").shift(1)).log().alias("OpenOfs") * 10000,
+            (pl.col("Volume") / pl.col("Volume").shift(1)).log().alias("VolumeOfs")
+            * 10000,
+        ]
+    )
+
+
+def _log_valiation_high_low_close_from_open(df: OHCLV):
+    return df.with_columns(
+        [
+            (pl.col(col) / pl.col("Open")).log().alias(f"{col}Ofs") * 10000
+            for col in COLS_HLC
+        ]
+    )
+
+
+def calc_ofs_ohclv(df: OHCLV) -> OFS_OHCLV:
+    df_ofs_ov = _log_valiation_volume_from_open(df)
+    df_ofs_hlc = _log_valiation_high_low_close_from_open(df)
+    # 結合して1行目を削除（shiftでNaNになるため）
+    return (
+        pl.concat(
+            [
+                df.select("Date"),
+                df_ofs_ov,
+                df_ofs_hlc.select([f"{col}Ofs" for col in COLS_HLC]),
+            ],
+            how="horizontal",
+        )
+        .slice(1)
+        .select(OFS_OHCLV_PLDF.columns)
+    )
+
+
+if __name__ == "__main__":
+    df = read_df_aapl()
+    df_ofs = calc_ofs_ohclv(df)
+    print(df_ofs)
