da053082e982f6cb86348cc348e22be14b09ba86
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-08 16:28:36 +0900

    コメント追加

--- tests/fixture/factory/feature/test_ofs_ohlcv.py
+++ tests/fixture/factory/feature/test_ofs_ohlcv.py
@@ -3,5 +3,14 @@ from fixture.factory.feature.ofs_ohlcv import factory_ofs_ohlcv
 
 
 def test_factory_ofs_ohlcv():
+    """
+    MEMO: factory ohlcvのテスト内容について
+        ofs_ohlcvの内容については、derive_ofs_ohlcvの方で検証する。
+        factoryの方は、インスタンスができてるかどうか？という最低限のテスト。
+
+        fixtureの基本方針として、fixtureについてのテストは最低限とする。
+        機能の品質は、各関数ごとのテストで担保されるべきという思想。
+        二重テスト状態はなるべく避けたい。
+    """
     ofs_ohlcv = factory_ofs_ohlcv()
     assert isinstance(ofs_ohlcv, OFS_OHLCV)
