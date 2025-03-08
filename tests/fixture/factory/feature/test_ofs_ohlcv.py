e2f55d9fbfdc0e4e6ca1d9f7a9f32ed80ecc7a76
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-08 13:15:20 +0900

    内容確認はderive側で判定すべきだろう

--- tests/fixture/factory/feature/test_ofs_ohlcv.py
+++ tests/fixture/factory/feature/test_ofs_ohlcv.py
@@ -5,4 +5,3 @@ from fixture.factory.feature.ofs_ohlcv import factory_ofs_ohlcv
 def test_factory_ofs_ohlcv():
     ofs_ohlcv = factory_ofs_ohlcv()
     assert isinstance(ofs_ohlcv, OFS_OHLCV)
-    # TODO: ofs_ohlcvの内容確認処理
