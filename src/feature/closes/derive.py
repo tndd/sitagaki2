4b3db4dc3f2c9e4496e93da734d2af3b58642a4a
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-10 18:36:03 +0900

    wip

--- src/feature/closes/derive.py
+++ src/feature/closes/derive.py
@@ -1,5 +1,6 @@
+from dataset.ohlcv.schema import OHLCV
 from feature.closes.schema import CLOSES_N4
 
 
-def derive_closes_n4() -> CLOSES_N4:
+def derive_closes_n4(df: OHLCV) -> CLOSES_N4:
     pass
