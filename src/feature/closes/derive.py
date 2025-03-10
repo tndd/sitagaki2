ce557b9d9a058d4daac8c9426d0057eba1431f03
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-10 18:54:21 +0900

    動いてるっぽいな

--- src/feature/closes/derive.py
+++ src/feature/closes/derive.py
@@ -1,6 +1,35 @@
+import polars as pl
+
+from common.const import SCALE_BP
 from dataset.ohlcv.schema import OHLCV
-from feature.closes.schema import CLOSES_N4
+from feature.closes.schema import CLOSES_N4, CLOSES_N4_PLDF
 
 
 def derive_closes_n4(df: OHLCV) -> CLOSES_N4:
-    pass
+    return (
+        df.select(
+            [
+                pl.col("Date"),
+                (pl.col("Close") / pl.col("Close").shift(1)).log().alias("now")
+                * SCALE_BP,
+                (pl.col("Close").shift(1) / pl.col("Close").shift(2))
+                .log()
+                .alias("lag_1")
+                * SCALE_BP,
+                (pl.col("Close").shift(2) / pl.col("Close").shift(3))
+                .log()
+                .alias("lag_2")
+                * SCALE_BP,
+                (pl.col("Close").shift(3) / pl.col("Close").shift(4))
+                .log()
+                .alias("lag_3")
+                * SCALE_BP,
+                (pl.col("Close").shift(4) / pl.col("Close").shift(5))
+                .log()
+                .alias("lag_4")
+                * SCALE_BP,
+            ]
+        )
+        .slice(5)
+        .select(CLOSES_N4_PLDF.columns)
+    )
