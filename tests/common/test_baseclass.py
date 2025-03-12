5bcda87462b872e801bbd353c441de0b44e87792
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-13 02:13:11 +0900

    baseclassのテスト更新

--- tests/common/test_baseclass.py
+++ tests/common/test_baseclass.py
@@ -1,20 +1,20 @@
-from polars import Date, Float64, Int64, Schema
+from polars import DataFrame, Date, Float64, Int64, Schema
 
 from common.baseclass import PLDF
 
 
 def test_PLDF():
-    pldf = PLDF(
-        schema=Schema(
-            {
-                "A_DT": Date,
-                "B_FL": Float64,
-                "C_IN": Int64,
-            }
-        ),
-        origins=None,
+    # クラス変数の定義
+    PLDF.schema = Schema(
+        {
+            "A_DT": Date,
+            "B_FL": Float64,
+            "C_IN": Int64,
+        }
     )
-    # インスタンスの作成が成功してるか
+    PLDF.origins = None
+    # インスタンス変数dfを宣言しつつ実体化
+    pldf = PLDF(df=DataFrame())
     assert isinstance(pldf, PLDF)
     # カラム名が定義の通りの並びになってるか？
-    assert pldf.columns == ["A_DT", "B_FL", "C_IN"]
+    assert pldf.get_col_names() == ["A_DT", "B_FL", "C_IN"]
