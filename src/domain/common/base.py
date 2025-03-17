abf5f73e5d82af5cccbde3a21c716ed8debe1014
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-17 15:46:08 +0900

    get_dataset_svの仮実装
    dateset_svは変数として持つというのも検討

--- src/domain/common/base.py
+++ src/domain/common/base.py
@@ -1,7 +1,40 @@
-from polars import DataFrame, Schema
+from dataclasses import dataclass
+
+from polars import DataFrame, Schema, Series
+
+
+@dataclass
+class DatasetSV:
+    """
+    教師ありデータセットのフォーマット
+    (SuperVised)
+    """
+
+    train: DataFrame
+    label: Series
+
+
+@dataclass
+class DatasetSVML:
+    """
+    複数ラベルの
+    教師ありデータセットのフォーマット
+    (SuperVised MultiLabel)
+    """
+
+    train: DataFrame
+    labels: DataFrame
 
 
 class Pldf:
+    """
+    Polars dataframeを扱うための抽象クラス
+
+    DFをそのまま使うと、単なるDataframe型となってしまい、
+    データの把握が困難になるため。
+    スキーマ定義と親の情報を持つ。
+    """
+
     SCHEMA: Schema
     ORIGIN: list["Pldf"] | None = None
 
@@ -14,3 +47,14 @@ class Pldf:
         カラム名のリストを取得する
         """
         return cls.SCHEMA.names()
+
+    def get_dataset_sv(self, label: str):
+        """
+        指定されたlabelを教師データカラムに。
+        そしてその他のカラムを学習データとして、
+        教師あり学習トレーニング用のDatasetSVに加工して返す。
+        """
+        return DatasetSV(
+            train=self.df.select(self.df.columns.exclude([label])),
+            label=self.df[label],
+        )
