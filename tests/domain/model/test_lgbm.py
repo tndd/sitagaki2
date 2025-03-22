979b521367a9665a7970b45ac61486741cd582cd
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-22 20:10:10 +0900

    train_model_lgbm_closes_n4のテスト実装
    簡易実装。というかこれどうやってテストするか？

---
+++ tests/domain/model/test_lgbm.py
@@ -0,0 +1,10 @@
+from lightgbm import Booster
+
+from domain.model.lgbm import train_model_lgbm_closes_n4
+from fixture.factory.feature.closes import factory_closes_n4
+
+
+def test_train_model_lgbm_closes_n4():
+    closes = factory_closes_n4()
+    model = train_model_lgbm_closes_n4(closes)
+    assert isinstance(model, Booster)
