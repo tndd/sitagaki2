af0b7146d8bb2af32c25eba0130c3868c0ef163e
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-30 01:01:03 +0900

    todo
    まずlabeledのファクトリから

--- tests/infra/model/test_labeled_dataset.py
+++ tests/infra/model/test_labeled_dataset.py
@@ -1,1 +1,11 @@
-# TODO: impl
+from infra.model.labeled_dataset import LabeledDataset, LabeledDatasetSplit
+
+
+def test_labeled_dataset():
+    # TODO: impl
+    pass
+
+
+def test_labeled_dataset_split():
+    # TODO: impl
+    pass
