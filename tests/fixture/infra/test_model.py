660315f389e121e91bc53eab2d61129bb27e79f2
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-31 01:40:11 +0900

    rename

--- tests/fixture/infra/test_model.py
+++ tests/fixture/infra/test_model.py
@@ -2,7 +2,7 @@ from fixture.infra.model.dataset import DatasetImpl, factory_dataset_impl
 from fixture.infra.model.feature import FeatureImpl, factory_feature_impl
 
 
-def test_factory_dataset():
+def test_factory_dataset_impl():
     dsi: DatasetImpl = factory_dataset_impl()
     assert isinstance(dsi, DatasetImpl)
     assert dsi.df.index.name == "Date"
