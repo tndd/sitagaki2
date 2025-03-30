607f1865db38e0bdfb6904f5fbdd2bb42c1903f8
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-30 16:59:02 +0900

    ohlcv2の新しい実装

--- src/domain/dataset/ohlcv2.py
+++ src/domain/dataset/ohlcv2.py
@@ -1,34 +1,19 @@
 from pandas import DataFrame
-from pandera import Column, DataFrameSchema
 
 from infra.model.dataset import Dataset
 
 
 class Ohlcv2(Dataset):
-    SCHEMA_OHLCV = {
-        "Open": Column(float),
-        "High": Column(float),
-        "Low": Column(float),
-        "Close": Column(float),
-        "Volume": Column(int),
+    SCHEMA = {
+        "Open": float,
+        "High": float,
+        "Low": float,
+        "Close": float,
+        "Volume": int,
     }
-    SCHEMA = SCHEMA_OHLCV
-    ORIGIN = None
 
     def __init__(self, df: DataFrame) -> None:
         """
         IndexはDateで固定
         """
         super().__init__(df, index="Date")
-
-    @classmethod
-    def get_col_names_ohlcv(cls) -> list[str]:
-        return list(cls.SCHEMA_OHLCV.keys())
-
-
-class Inherited(Ohlcv2):
-    SCHEMA = DataFrameSchema(
-        {
-            "Next": Column(float),
-        }
-    )
