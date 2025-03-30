9b85793dda29f9de739919b9566afeb216132f81
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-31 01:40:05 +0900

    test_factory_feature_implの実装

--- tests/fixture/infra/test_model.py
+++ tests/fixture/infra/test_model.py
@@ -1,7 +1,14 @@
 from fixture.infra.model.dataset import DatasetImpl, factory_dataset_impl
+from fixture.infra.model.feature import FeatureImpl, factory_feature_impl
 
 
 def test_factory_dataset():
     dsi: DatasetImpl = factory_dataset_impl()
     assert isinstance(dsi, DatasetImpl)
     assert dsi.df.index.name == "Date"
+
+
+def test_factory_feature_impl():
+    feature: FeatureImpl = factory_feature_impl()
+    assert isinstance(feature, FeatureImpl)
+    assert feature.df.index.name == "Date"
