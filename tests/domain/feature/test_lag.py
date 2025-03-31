087e04a47568905fcb12cf2deed84bbabf539ea6
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-04-01 01:22:48 +0900

    テスト機能をfactoryから移管
    factoryは最低限のテスト。
    クラスの内容検証はクラス側のテストで行うべき。

--- tests/domain/feature/test_lag.py
+++ tests/domain/feature/test_lag.py
@@ -1,1 +1,21 @@
-# TODO: テストを書く
+from fixture.domain.feature.lag import factory_lag_closes10
+
+
+def test_lag_closes10():
+    lag = factory_lag_closes10()
+    assert lag.field.index == "Date"
+    assert lag.field.label == ["l0"]
+    assert lag.field.exclude == []
+    assert lag.field.col_names == [
+        "l0",
+        "l1",
+        "l2",
+        "l3",
+        "l4",
+        "l5",
+        "l6",
+        "l7",
+        "l8",
+        "l9",
+        "l10",
+    ]
