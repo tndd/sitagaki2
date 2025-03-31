364d5fafbfd1fa71af53c38dce022520550cc80a
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-03-31 15:24:48 +0900

    こっちの方が優先度は高いだろう

--- src/infra/model/feature.py
+++ src/infra/model/feature.py
@@ -29,6 +29,13 @@ class Feature(Dataset):
             exclude=exclude,
         )
 
+    def build_df(self, dataset: Dataset | list[Dataset]) -> DataFrame:
+        """
+        データセットから特徴量を生成する抽象クラス。
+        単数か複数のdatasetから、featureのdataframeを組み上げる。
+        """
+        raise NotImplementedError("feature/build_df is not implemented.")
+
     def merge_df(self) -> DataFrame:
         if not isinstance(self.dataset, list):
             # 単数のdatasetの場合
@@ -41,11 +48,3 @@ class Feature(Dataset):
             # 複数のdatasetの場合
             # TODO: 複数のdatasetを結合する処理を実装する
             raise NotImplementedError("まだ未実装")
-
-    @staticmethod
-    def build_df(dataset: Dataset | list[Dataset]) -> DataFrame:
-        """
-        データセットから特徴量を生成する抽象クラス。
-        単数か複数のdatasetから、featureのdataframeを組み上げる。
-        """
-        raise NotImplementedError("feature/build_df is not implemented.")
