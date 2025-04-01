f9e57319f3ed655534a43d23d3e9d653f9f76ee9
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-04-01 22:04:22 +0900

    1分足の場合、bp表記にしないと、値が小さくなりすぎるな

--- src/domain/feature/lag.py
+++ src/domain/feature/lag.py
@@ -42,18 +42,19 @@ class LagCloses10(OhlcvFeature):
         10日分の終値の対数差分の特徴量を生成する。
         欠損値を含む行は削除する。
         """
+        SCALE = 10000 # BP表記
         df = ohlcv.df.copy()  # コピーを作成して元データを変更しないようにする
         close = df["Close"]
         # 対数差分での変化率（日次リターン）を計算
-        df["l0"] = log(close / close.shift(1))
-        df["l1"] = log(close.shift(1) / close.shift(2))
-        df["l2"] = log(close.shift(2) / close.shift(3))
-        df["l3"] = log(close.shift(3) / close.shift(4))
-        df["l4"] = log(close.shift(4) / close.shift(5))
-        df["l5"] = log(close.shift(5) / close.shift(6))
-        df["l6"] = log(close.shift(6) / close.shift(7))
-        df["l7"] = log(close.shift(7) / close.shift(8))
-        df["l8"] = log(close.shift(8) / close.shift(9))
-        df["l9"] = log(close.shift(9) / close.shift(10))
-        df["l10"] = log(close.shift(10) / close.shift(11))
+        df["l0"] = log(close / close.shift(1)) * SCALE
+        df["l1"] = log(close.shift(1) / close.shift(2)) * SCALE
+        df["l2"] = log(close.shift(2) / close.shift(3)) * SCALE
+        df["l3"] = log(close.shift(3) / close.shift(4)) * SCALE
+        df["l4"] = log(close.shift(4) / close.shift(5)) * SCALE
+        df["l5"] = log(close.shift(5) / close.shift(6)) * SCALE
+        df["l6"] = log(close.shift(6) / close.shift(7)) * SCALE
+        df["l7"] = log(close.shift(7) / close.shift(8)) * SCALE
+        df["l8"] = log(close.shift(8) / close.shift(9)) * SCALE
+        df["l9"] = log(close.shift(9) / close.shift(10)) * SCALE
+        df["l10"] = log(close.shift(10) / close.shift(11)) * SCALE
         return df.dropna()  # 欠損値を含む行を削除して返す
