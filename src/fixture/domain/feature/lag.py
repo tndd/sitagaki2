c40312325b5e38ebf73fcea5267179b57c3540bd
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2025-04-01 21:07:52 +0900

    nを指定できるように

--- src/fixture/domain/feature/lag.py
+++ src/fixture/domain/feature/lag.py
@@ -2,6 +2,6 @@ from domain.feature.lag import LagCloses10
 from fixture.domain.dataset.ohlcv import factory_ohlcv_random_walk
 
 
-def factory_lag_closes10() -> LagCloses10:
-    ohlcv = factory_ohlcv_random_walk(n=1000)
+def factory_lag_closes10(n: int = 1000) -> LagCloses10:
+    ohlcv = factory_ohlcv_random_walk(n)
     return LagCloses10(ohlcv)
