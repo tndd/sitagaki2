0dd016d15146bd4aa8c1691e8e16b9b0b150b837
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-04-17 23:45:02 +0900

    合成dfのカラム検証

--- tests/domain/feature/test_common.py
+++ tests/domain/feature/test_common.py
@@ -24,3 +24,30 @@ def test_ohlcv_feature():
     assert ohlcv_feature.field.label == ["OF0"]
     assert ohlcv_feature.field.exclude == ["OF5", "OF6"]
     assert ohlcv_feature.field.feature_names == ["OF1", "OF2", "OF3", "OF4"]
+    # ohlcv+namesのカラム名リスト => インデックスDateのみ除外されてる
+    assert ohlcv_feature.df_with_ohlcv.columns.tolist() == [
+        "Open",
+        "High",
+        "Low",
+        "Close",
+        "Volume",
+        "OF0",
+        "OF1",
+        "OF2",
+        "OF3",
+        "OF4",
+        "OF5",
+        "OF6",
+    ]
+    # ohlcv+features_nameのカラム名リスト => インデックスDate & labelが除外されてる
+    assert ohlcv_feature.df_feature_with_ohlcv.columns.tolist() == [
+        "Open",
+        "High",
+        "Low",
+        "Close",
+        "Volume",
+        "OF1",
+        "OF2",
+        "OF3",
+        "OF4",
+    ]
