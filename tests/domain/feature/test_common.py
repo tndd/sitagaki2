6526b9ae7f2ec4113e6cf1592c1a8d07718effac
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-04-17 23:40:11 +0900

    fixtureで行なってたテストを分割
    fixtureではインスタンス作成の確認のみ

---
+++ tests/domain/feature/test_common.py
@@ -0,0 +1,26 @@
+from fixture.domain.dataset.ohlcv import factory_ohlcv
+from fixture.domain.feature.common import OhlcvFeatureImpl
+
+
+def test_ohlcv_feature():
+    """
+    OhlcvFeature自体のテスト
+    fixtureの方では行わなかった、インスタンスへの詳細なテストを行う
+    """
+    ohlcv = factory_ohlcv()
+    ohlcv_feature = OhlcvFeatureImpl(ohlcv)
+    assert isinstance(ohlcv_feature, OhlcvFeatureImpl)
+    assert ohlcv_feature.df.index.name == "Date"
+    assert ohlcv_feature.df.index.dtype == "datetime64[ns]"
+    assert ohlcv_feature.field.names == [
+        "OF0",
+        "OF1",
+        "OF2",
+        "OF3",
+        "OF4",
+        "OF5",
+        "OF6",
+    ]
+    assert ohlcv_feature.field.label == ["OF0"]
+    assert ohlcv_feature.field.exclude == ["OF5", "OF6"]
+    assert ohlcv_feature.field.feature_names == ["OF1", "OF2", "OF3", "OF4"]
--- tests/fixture/domain/feature/test_common.py
+++ tests/fixture/domain/feature/test_common.py
@@ -3,20 +3,10 @@ from fixture.domain.feature.common import OhlcvFeatureImpl
 
 
 def test_factory_ohlcv_feature_impl():
+    """
+    OhlcvFeatureImplのファクトリのテスト
+    テスト内容はインスタンスが作れているか最低限のみ
+    """
     ohlcv = factory_ohlcv()
     ohlcv_feature = OhlcvFeatureImpl(ohlcv)
     assert isinstance(ohlcv_feature, OhlcvFeatureImpl)
-    assert ohlcv_feature.df.index.name == "Date"
-    assert ohlcv_feature.df.index.dtype == "datetime64[ns]"
-    assert ohlcv_feature.field.names == [
-        "OF0",
-        "OF1",
-        "OF2",
-        "OF3",
-        "OF4",
-        "OF5",
-        "OF6",
-    ]
-    assert ohlcv_feature.field.label == ["OF0"]
-    assert ohlcv_feature.field.exclude == ["OF5", "OF6"]
-    assert ohlcv_feature.field.feature_names == ["OF1", "OF2", "OF3", "OF4"]
