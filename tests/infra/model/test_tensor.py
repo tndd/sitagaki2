dff230001c1d7bc44cb53b8e2dbee813e5fc2cb7
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-05-04 18:54:08 +0900

    test_tensorの簡易実装
    todo: 分割についての確認

--- tests/infra/model/test_tensor.py
+++ tests/infra/model/test_tensor.py
@@ -1,8 +1,22 @@
-def test_labeled_dataset():
-    # LATER: impl
-    pass
+from fixture.infra.model.dataset import (
+    DatasetImpl,
+    factory_labeled_dataset_impl,
+)
+from infra.model.tensor import (
+    LabeledTensor,
+    SplitLabeledTensor,
+    convert_labeled_dataset_to_split_tensor,
+    convert_labeled_dataset_to_tensor,
+)
 
 
-def test_labeled_dataset_split():
-    # LATER: impl
-    pass
+def test_convert_labeled_dataset_to_tensor():
+    ldsi: DatasetImpl = factory_labeled_dataset_impl()
+    labeled_tensor = convert_labeled_dataset_to_tensor(ldsi)
+    assert isinstance(labeled_tensor, LabeledTensor)
+
+
+def test_convert_labeled_dataset_to_split_tensor():
+    ldsi: DatasetImpl = factory_labeled_dataset_impl()
+    split_labeled_tensor = convert_labeled_dataset_to_split_tensor(ldsi)
+    assert isinstance(split_labeled_tensor, SplitLabeledTensor)
