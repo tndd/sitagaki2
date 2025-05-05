b6092af9f8664797be0ee100eea723e1a28e28ec
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-05-05 22:13:18 +0900

    rename

--- tests/fixture/infra/test_model.py
+++ tests/fixture/infra/test_model.py
@@ -9,25 +9,25 @@ from fixture.infra.model.dataset import (
 
 
 def test_factory_dataset_impl():
-    dsi: DatasetImpl = factory_dataset_impl()
-    assert isinstance(dsi, DatasetImpl)
-    dsi.pandera_schema.validate(dsi.df)
-    assert dsi.columns == ["Open", "High", "Low", "Close", "Volume"]
-    assert dsi.index == "Date"
+    dataset: DatasetImpl = factory_dataset_impl()
+    assert isinstance(dataset, DatasetImpl)
+    dataset.pandera_schema.validate(dataset.df)
+    assert dataset.columns == ["Open", "High", "Low", "Close", "Volume"]
+    assert dataset.index == "Date"
 
 
 def test_factory_dataset_impl_v2():
-    dsi: DatasetImplV2 = factory_dataset_impl_v2()
-    assert isinstance(dsi, DatasetImplV2)
-    dsi.pandera_schema.validate(dsi.df)
-    assert dsi.columns == ["D0", "D1", "D2", "D3", "D4"]
-    assert dsi.index == "Date"
+    dataset_v2: DatasetImplV2 = factory_dataset_impl_v2()
+    assert isinstance(dataset_v2, DatasetImplV2)
+    dataset_v2.pandera_schema.validate(dataset_v2.df)
+    assert dataset_v2.columns == ["D0", "D1", "D2", "D3", "D4"]
+    assert dataset_v2.index == "Date"
 
 
 def test_factory_labeled_dataset_impl():
-    ldsi: LabeledDatasetImpl = factory_labeled_dataset_impl()
-    assert isinstance(ldsi, LabeledDatasetImpl)
-    ldsi.pandera_schema.validate(ldsi.df)
-    assert ldsi.columns == ["F0", "F1", "F2", "F3", "L"]
-    assert ldsi.index == "Date"
-    assert ldsi.label == "L"
+    labeled_dataset: LabeledDatasetImpl = factory_labeled_dataset_impl()
+    assert isinstance(labeled_dataset, LabeledDatasetImpl)
+    labeled_dataset.pandera_schema.validate(labeled_dataset.df)
+    assert labeled_dataset.columns == ["F0", "F1", "F2", "F3", "L"]
+    assert labeled_dataset.index == "Date"
+    assert labeled_dataset.label == "L"
