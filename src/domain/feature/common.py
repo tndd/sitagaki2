dbca8ee9e9945f4b8d10af5861dc260b80b08ff4
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-05-05 21:18:23 +0900

    wip: ohclv_featureはこれくらい簡素化してもいいかもしれない

--- src/domain/feature/common.py
+++ src/domain/feature/common.py
@@ -1,30 +1,23 @@
-from typing import Protocol
-
-from pandas import DataFrame, concat
+from pandas import DataFrame
 from pandera.api.pandas.types import PandasDtypeInputTypes as PanderaType
 
-from domain.dataset.ohlcv import Ohlcv
 from infra.model.dataset import LabeledDataset
-
-
-class OhlcvFeatureGenerator(Protocol):
-    """
-    ohlcvを元に特徴量を生成するための抽象クラス。
-
-    ohlcv以外にも引数を受け取ることも想定しているため、
-    クラス変数を取り得るので注意。
-    """
-
-    def generate(self, ohlcv: Ohlcv) -> DataFrame:
-        """
-        ohlcvから特徴量を生成する関数。
-        """
-        ...
 
 
 class OhlcvFeature(LabeledDataset):
     """
     ohlcvを元として生成される特徴量を表すクラス。
+
+    Props:
+        df:
+            ohlcvと特徴量のカラムを持つデータフレーム。
+        definition:
+            dfのカラムの型定義。
+            ただしここは特徴量の定義のみを含む。
+            ohlcvの定義については含んではいない。
+        label:
+            dfのラベルカラム名。
+
 
     ohlcvを要素として持つ理由:
         バックテストで特徴量のパフォーマンスの検証を行うため。
@@ -34,56 +27,33 @@ class OhlcvFeature(LabeledDataset):
         今のところdfのカラムはデフォルトでは特徴量の分しか持っていない。
         だが本当はohlcvのものもデフォルトで持たせて、
         必要に応じて特徴量のみに制限するという運用の方が適切ではないだろうか？
+
+    MEMO: 設計方針
+        dfにはohlcvのものも含める。
+        ただし、特徴量のみのdfを返すメソッドを用意しておく。
+        ohlcvのカラムは決まったものであるため、それの実現は容易だ。
+
+        これによってdf生成メソッドのややこしさ問題を回避できるだろう。
+        それを考えることなく、完成済みのdfを受け取るだけで話が済む。
     """
 
     def __init__(
         self,
-        ohlcv: Ohlcv,
-        generator: OhlcvFeatureGenerator,
+        df: DataFrame,
         definition: dict[str, PanderaType],
         label: str,
     ) -> None:
-        """
-        ohlcvを受け取り、特徴量を生成する。
-        """
-        self.ohlcv = ohlcv
         super().__init__(
-            df=generator.generate(ohlcv),
+            df=df,
             definition=definition,
             label=label,
             index="Date",
         )
 
     @property
-    def df_with_ohlcv(self) -> DataFrame:
-        """
-        自身の特徴量dfとohlcv.dfを結合して返す。
-        """
-        return concat(
-            [self.ohlcv.df, self.df],
-            axis=1,
-            join="inner",
-        )
-
-    @property
-    def df_feature_with_ohlcv(self) -> DataFrame:
-        """
-        ohlcv含めてカラムを特徴量のみに絞って返す。
-        バックテストで使われるのはこちらが想定される。
-        """
-        return concat(
-            [
-                self.ohlcv.df,
-                self.df.loc[:, self.non_label_columns],
-            ],
-            axis=1,
-            join="inner",
-        )
-
-    @property
-    def df_feature(self) -> DataFrame:
+    def feature_df(self) -> DataFrame:
         """
         特徴量のみのデータフレームを返す。
         学習済みのモデルに与えるための値として使う。
         """
-        return self.df.loc[:, self.non_label_columns]
+        return self.df.loc[:, list(self.definition.keys())]
