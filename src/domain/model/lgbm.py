d8d47cb6efaf7cab9206f25b54f2af2d51e7ae75
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-05-14 17:37:05 +0900

    feat(lgbm): 既存モデルを初期値として指定可能に
    train_lgbm_with_ohlcv_feature関数にinit_modelパラメータを追加し、既存のモデルを初期値として指定できるようにしました。これにより、モデルの継続的な学習が可能になります。

--- src/domain/model/lgbm.py
+++ src/domain/model/lgbm.py
@@ -8,7 +8,10 @@ from domain.feature.common import OhlcvFeature
 from infra.model.tensor import convert_labeled_dataset_to_split_tensor
 
 
-def train_lgbm_with_ohlcv_feature(dataset: OhlcvFeature) -> Booster:
+def train_lgbm_with_ohlcv_feature(
+    dataset: OhlcvFeature,
+    init_model: Booster | None = None,
+) -> Booster:
     params = {
         "objective": "regression",  # 回帰問題として解く
         "metric": "rmse",  # 評価指標
@@ -28,4 +31,5 @@ def train_lgbm_with_ohlcv_feature(dataset: OhlcvFeature) -> Booster:
         valid_sets=[lgb_test],
         num_boost_round=500,  # ラウンド数
         callbacks=[early_stopping(50)],  # 早期停止の監視期間
+        init_model=init_model,  # 既存モデルを指定
     )
